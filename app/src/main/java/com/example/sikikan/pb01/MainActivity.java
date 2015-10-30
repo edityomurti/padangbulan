b7219ff7913385522672e4e7dc12dc7f0e0be962
hanifsgy <hanifsgy@gmail.com>
2015-10-30 10:45:52 +0700

    isih akeh sek salah

--- app/src/main/java/com/example/sikikan/pb01/MainActivity.java
+++ app/src/main/java/com/example/sikikan/pb01/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.sikikan.pb01;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    public Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +18,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        toolbar= (Toolbar) findViewById(R.id.app_bar);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
-        }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // if action bar click, action bar show
+        // automatically handle clicks
+        // parent di AndroidManifest.xml
+
+        int id = item.getItemId();
+
+
+        if (id == R.id.add) {
+
+            startActivity(new Intent(this, ActivityAmbilPeran.class));
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
